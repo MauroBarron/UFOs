f752449d73d250123e65dba49229fc260bfc7937
unknown <josemaurobarron@gmail.com>
2020-02-19 16:10:14 -0800

    Code save. Module 11.6 Add the Data and customize with Bootstrap and CSS

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-// import the data form data.js
+// import the data from data.js
 const tableData = data;
 
 //Reference the HTML table using d3
@@ -7,23 +7,22 @@ var tbody = d3.select("tbody");
 // build Table function
 // First clear out existing data
 function buildTable(data){
-   tbody.html("") 
-};
-// Loop thru each object in data
-// append a row and cells for each value
-// in each row
-data.forEach((dataRow) => {
-    //Append row to table body
-    let row=tbody.append("tr")
-    //Loop thru each field in dataRow and add
-    // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-        }
-    );
-});
-
+   tbody.html("");
+    // Then loop thru each object in data
+    // append a row and cells for each value
+    // in each row
+    data.forEach((dataRow) => {
+        //Append row to table body
+        let row=tbody.append("tr");
+        //Loop thru each field in dataRow and add
+        // each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
+}
 // Filter by click function
 function handleClick () {
     // Get datetime value from filter
